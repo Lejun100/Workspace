7ddb23bc066bc465123f59f9d3475705dbc48bf5
Lejun100 <lejunsak@email.com>
2023-10-19 21:34:57 -0300

    licao

---
+++ JS/Aula00-RaciocinioLogico/Aula03-string/Exerciciocasa/main.js
@@ -0,0 +1,15 @@
+const string01 = 'Estamos aprendendo Javascript e seremos futuros Devs'
+console.log(string01.charAt(19));
+
+let texto = 'abcdefghijklmnopqrstuvwxyz';
+console.log(texto.length);
+
+const s = 'Estamos aprendendo Javascript e seremos futuros Devs';
+console.log(s.toUpperCase());
+
+
+var abc = 'Estamos aprendendo Javascript e seremos futuros Devs ';
+console.log(abc.substring(48,52));
+
+
+
---
+++ JS/Aula00-RaciocinioLogico/Aula03-string/main.js
@@ -0,0 +1,48 @@
+var texto1 = "Dia 31 de outubro terá a premiação da bola de ouro"
+
+var texto2 = "e espero que o vencedor seja Lionel Andres Messi"
+
+console.log(`${texto1}, além dos indicados a bola de ouro temos muitos favoritos ${texto2}`)
+
+
+var numero1 = 10;
+
+var numero2 = 30;
+
+console.log(`${numero1-numero2}`)
+
+//strings sempre começam na posição 0//
+
+console.clear()
+
+const string01 = 'Instituto da Oportunidade Social';
+
+console.log(string01.charAt(10));
+console.log(string01[10]);
+
+
+let texto = 'abcdefghijklmnopqrstuvwxyz';
+console.log(texto.length);
+
+
+const string02 = "Instituto da Oportunidade Social"
+
+console.log("Quantidade de caracteres: " + string02.length)
+
+const s = "Hello World!";
+
+console.log(s.toUpperCase())
+console.log(s.toLocaleLowerCase())
+
+
+
+const str = 'Mozilla';
+
+
+console.log(str.substring(2,5));
+console.log(str.substring(0));
+
+
+
+
+
