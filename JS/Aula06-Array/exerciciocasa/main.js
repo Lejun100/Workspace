196c4c36d423fc8278244754267c5bc9be092ed3
Lejun100 <143458724+Lejun100@users.noreply.github.com>
2023-10-27 20:38:30 -0300

    licao feita

--- JS/Aula06-Array/exerciciocasa/main.js
+++ JS/Aula06-Array/exerciciocasa/main.js
@@ -17,6 +17,33 @@ console.log(abc)
 
 
 
+var bdml = [
+    ["HTML",1993,CSS,1996]
+    ["Bootstrap",2011,JS,1995]
+    ["React",2013,"Java",1995]
+]
+
+console.log(bdml)
+
+console.log(bdml.length)
+
+let retiraPosicao = bdml[2.1]
+console.log(retiraPosicao)
+
+console.log(`Antes: ${bdml}`);
+bdml[1,3] = 'JavaScript';
+console.log(`Depois: ${bdml}`);
+
+delete bdml[2,2]
+console.log(bdml);
+
+
+let final = bdml.push('pipoca')
+console.log(final)
+
+
+
+
 
 //let receba = numArray.toString()
 //console.log(receba)
