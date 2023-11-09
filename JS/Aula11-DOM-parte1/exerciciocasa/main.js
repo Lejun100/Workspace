78bcb667ed0ff591e8df5d291fef57eb758d8d6e
Lejun100 <lejunsak@email.com>
2023-11-08 21:22:15 -0300

    abc

---
+++ JS/Aula11-DOM-parte1/exerciciocasa/main.js
@@ -0,0 +1,14 @@
+const h1 = document.createElement('h1');
+
+h1.textContent = 'cadastro do usuário';
+
+document.body.appendChild(h1);
+
+document.title = 'cadastro empresa Macron'
+
+
+
+
+
+
+
---
+++ JS/Aula11-DOM-parte1/main.js
@@ -0,0 +1,34 @@
+// document.createElement("BUTTON");
+
+let botao = document.createElement("BUTTON");
+let corpo = document.body;
+
+document.body.appendChild(botao)
+
+botao.innerHTML = 'clique no meu botão';
+
+document.body.style.backgroundColor = '#F09AB7'
+botao.style.border = 'solid 20px red';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(window)
+// window.alert("Vamos começar o dom, ANKARA MESSIIII!!!!!!!!!")
+// alert("Messi")
