07c1c7b49918adfd4eb06d8cce2eeb6e638362ae
Lejun100 <lejunsak@email.com>
2023-11-10 20:31:39 -0300

    abc

--- JS/Aula13-DOM-parte3/main.js
+++ JS/Aula13-DOM-parte3/main.js
@@ -12,8 +12,42 @@ while(c < pipoca.length){
 
 
 
+function abc(){
+    var entradaNome = prompt('Insira seus dados bancários')
+    let mostraNome =  document.querySelector('#mostraNome')
+    console.log(entradaNome)
 
 
+
+    mostraNome.innerHTML = `Olá ${entradaNome} Bem vindo a mim`
+
+}
+
+const Nome = () => {
+    let recebeNome = document.querySelector('#recebeNome')
+    let mostraNome =  document.querySelector('#mostraNome')
+    console.log(recebeNome)
+    mostraNome.innerHTML = `Olá ${recebeNome.value} Bem vindo a minha house papai`
+}
+
+
+
+let evento = document.querySelector('#messi')
+evento.addEventListener('click, Nome')
+//o nome do event, callback(função), opcional(booleano true or false)
+
+const Nome() => {
+    let recebeNome = document.querySelector('#recebeNome')
+    let mostraNome = document.querySelector('#mostraNome')
+
+mostraNome.innerHTML = `Olá ${recebeNome.value} o Messi é o melhor do mundo`
+
+
+
+}
+
+
+evento.addEventListener('click, Nome ')
 
 
 
