7c615cdf8e03120ab81af688e47856f146d4f6ab
Lejun100 <lejunsak@email.com>
2023-11-17 20:01:43 -0300

    a

--- React/primeiro-projeto-react/src/App.js
+++ React/primeiro-projeto-react/src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Exemplo from './components/Exemplo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+   <Exemplo/>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header> */}
+
